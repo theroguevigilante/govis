f787d1245b93b24dcff6e1062be1e31bb1256905
theroguevigilante <naimish82@proton.me>
2026-06-29 18:13:17 +0530

    Remove dead code(Round1Data) and dep(udigest)

--- src/types.rs
+++ src/types.rs
@@ -1,13 +1,4 @@
 use generic_ec::{Point, SecretScalar, curves::Secp256k1};
-use udigest::Digestable;
-
-#[derive(Digestable)]
-pub struct Round1Data<'a> {
-    pub sid: &'a [u8],
-    pub i: u16,
-    pub x_public: &'a [Point<Secp256k1>],
-    pub y_public: &'a [Point<Secp256k1>],
-}
 
 pub struct DkgShares {
     pub commitments: Vec<Point<Secp256k1>>,
